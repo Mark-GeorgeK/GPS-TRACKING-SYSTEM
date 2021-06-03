8ed6896948b17923a8400846051bca021a5e8238
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2021-06-03 14:18:32 +0200

    Add
    test case 1 for 7 segment and led

--- Source.c
+++ Source.c
@@ -1,10 +1,21 @@
 #include "Functions.h"
+void delay_1sec(void)
+ {
+	 unsigned long i;
+    for( i = 0; i <= 3000000; i++ )
+        {}
+ }
 
 int main() {
 	//Testing SegmentsDisplay , Expected to find (0x56 on Port B, 0x40 on Port A)
-	int distance = 456;
+	int distance = 95;
 	Init();
-	SegmentsDisplay(distance);
 	
-	return 0;
+	while(!DestinationReached(distance)){
+		distance++;
+		SegmentsDisplay(distance);
+	  LED_ON(distance);
+		delay_1sec();
+	}
+	while(1){}
 }
