f2df19d380460f7a2b1c5825d40a8cffff76f03d
Hazem Hisham <hazemhesham10920@gmail.com>
2021-06-03 14:31:06 +0200

    Add

--- Source.c
+++ Source.c
@@ -8,14 +8,21 @@ void delay_1sec(void)
 
 int main() {
 	//Testing SegmentsDisplay , Expected to find (0x56 on Port B, 0x40 on Port A)
-	int distance = 95;
+	int distance = 0;
 	Init();
 	
-	while(!DestinationReached(distance)){
-		distance++;
-		SegmentsDisplay(distance);
-	  LED_ON(distance);
-		delay_1sec();
-	}
+	
+
+	//Test case #1: Testing 7 segments functions and LED_ON functions
+	// while(!DestinationReached(distance)){
+	// 	distance++;
+	// 	SegmentsDisplay(distance);
+	//   LED_ON(distance);
+	// 	delay_1sec();
+	// }
+
+	//Test case #2: Testing 7 segments functions and Distance functions
+	distance=DistanceBetween2Points(30.064601,31.277308,30.064693,31.277582);
+	void SegmentsDisplay(distance);
 	while(1){}
 }
